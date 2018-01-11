e1c77ef259ce891694b8d3defa4f903d2f093d2d
SlandShow <SlandShow12345@mail.ru>
2018-01-12 00:20:45 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+ * Хэширование строк
+ * Определённого размера
+ *
+ */
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Hash.h"
+
+using namespace std;
+using namespace hash;
+
+int main() {
+
+    Hash hash;
+
+    string str;
+    int lenght;
+
+    cin >> str;
+    cin >> lenght;
+    cout << hash.getHash(str, lenght);
+    return 0;
+}
